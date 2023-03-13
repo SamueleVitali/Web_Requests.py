b6bc2d61dfd32f2a57134f2386989845decea53f
Samuele <samuvit42@gmail.com>
2023-03-13 19:56:59 +0100

    1.2

---
+++ webreq.py
@@ -0,0 +1,94 @@
+import requests
+import validators
+
+print("""\
+
+██╗    ██╗███████╗██████╗ ██████╗ ███████╗ ██████╗ 
+██║    ██║██╔════╝██╔══██╗██╔══██╗██╔════╝██╔═══██╗
+██║ █╗ ██║█████╗  ██████╔╝██████╔╝█████╗  ██║   ██║
+██║███╗██║██╔══╝  ██╔══██╗██╔══██╗██╔══╝  ██║▄▄ ██║
+╚███╔███╔╝███████╗██████╔╝██║  ██║███████╗╚██████╔╝
+ ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝╚══════╝ ╚══▀▀═╝ 
+                                            
+              ┌─┐┬  ┌─┐┬ ┬┌─┐
+              ├─┤│  ├─┘├─┤├─┤
+              ┴ ┴┴─┘┴  ┴ ┴┴ ┴ (1.2)
+               
+               
+               
+                                      
+                    """)
+
+ver=False
+while not ver:
+    input_url = input("Enter the URL: ")
+
+    if not validators.url(input_url):
+        print("""
+        
+        ⚠ WARNING! ⚠ : Invalid URL!
+        
+        """)
+    else:
+        ver=True
+
+print("""\
+    
+1) GET
+2) POST
+    """)
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    print("""\
+    
+1) Simple GET
+2) GET + Cookie
+3) GET + Payload
+4) GET + Custom Header
+
+    """)
+
+    choice2 = int(input("\nEnter your choice: "))
+    if choice2 == 1:
+        r = requests.get(input_url)
+        
+    elif choice2 == 2:
+        cookiename = input("\nEnter your cookie name: ")
+        cookievalue = input("\nEnter your cookie value: ")
+        cookies = {cookiename: cookievalue}
+        r = requests.get(input_url, cookies=cookies)
+        
+    elif choice2 == 3:
+        PayloadParamName = input("\nEnter your Payload name: ")
+        PayloadParamValue = input("\nEnter your Payload value: ")
+        cookies = {PayloadParamName: PayloadParamValue}
+        r = requests.get(input_url, params=cookies)
+        
+    elif choice2 == 4:
+        HeaderName = input("\nEnter your Header name: ")
+        HeaderValue = input("\nEnter your Header value: ")
+        headers = {HeaderName: HeaderValue}
+        r = requests.get(input_url, headers=headers)
+        
+    print("\nRESPONSE:\n\n"+r.text)
+
+if choice == 2:
+    print("""\
+    
+1) Simple POST
+2) POST + Cookie
+    
+    """)
+
+    choice2 = int(input("\nEnter your choice: "))
+    if choice2 == 1:
+        r = requests.post(input_url)
+        print(r.text)
+    elif choice2 == 2:
+        cookiename = input("\nEnter your cookie name: ")
+        cookievalue = input("\nEnter your cookie value: ")
+        cookies = {cookiename: cookievalue}
+        r = requests.post(input_url, cookies=cookies)
+        print("\n"+r.text)
